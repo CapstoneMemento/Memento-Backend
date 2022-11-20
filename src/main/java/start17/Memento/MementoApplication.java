59051d74ef5ae60dcc12f211280aad6f7d3c3331
jyjyeon <k-j_y@naver.com>
2022-11-20 14:39:35 +0900

    Initial commit

---
+++ src/main/java/start17/Memento/MementoApplication.java
@@ -0,0 +1,13 @@
+package start17.Memento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/start17/Memento/MementoApplicationTests.java
@@ -0,0 +1,13 @@
+package start17.Memento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
