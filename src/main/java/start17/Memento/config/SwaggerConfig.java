64840427bcde925d8a290fc108d7b258e192370b
jyjyeon <k-j_y@naver.com>
2023-02-13 20:31:47 +0900

    feat: API 자동 문서화 환경세팅

---
+++ src/main/java/start17/Memento/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package start17.Memento.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableAutoConfiguration
+public class SwaggerConfig {
+
+    private final String version = "v1";
+
+    @Bean
+    public Docket commonApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(version)
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.story.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Memento API")
+                .description("Memento API 문서")
+                .version(version)
+                .build();
+    }
+}
