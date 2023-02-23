cdc0295562ae92ab6f187b6849befd566a42e8b9
jyjyeon <k-j_y@naver.com>
2023-02-23 15:53:31 +0900

    feat: 로그인 틀 구현

--- src/main/java/start17/Memento/controller/UserController.java
+++ src/main/java/start17/Memento/controller/UserController.java
@@ -3,15 +3,19 @@ package start17.Memento.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import start17.Memento.model.dto.User;
+import start17.Memento.domain.User;
+import start17.Memento.model.dto.UserDto;
 import start17.Memento.service.UserService;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Api(tags = "Users")
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -23,6 +27,21 @@ public class UserController {
     @PostMapping("/users/register")
     public String register(@RequestBody User user) throws IOException {
         userService.createUser(user);
+        log.info("register success");
         return "Register success";
+    }
+
+    @ApiOperation(value = "로그인", notes = "회원 id, 비밀번호를 입력해 로그인한다.")
+    @PostMapping("/users/login")
+    public String login(@RequestBody UserDto.LoginInfo user) throws IOException {
+
+        User login_user = userService.loginCheck(user);
+
+        if(login_user != null) {
+            log.info("login success");
+            return "Login success";
+        }
+        log.info("login fail");
+        return "redirect:/users/login";
     }
 }
--- src/main/java/start17/Memento/service/UserService.java
+++ src/main/java/start17/Memento/service/UserService.java
@@ -1,11 +1,14 @@
 package start17.Memento.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import start17.Memento.model.dao.UserMapper;
-import start17.Memento.model.dto.User;
+import start17.Memento.domain.User;
+import start17.Memento.model.dto.UserDto;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -13,6 +16,18 @@ public class UserService {
 
     public void createUser(User user) {
         userMapper.createUser(user);
+    }
+
+    public User loginCheck(UserDto.LoginInfo user_dto) {
+
+        User user = userMapper.getUserById(user_dto.getUser_id());
+
+        if(user_dto.getPassword().equals(user.getPassword())) {
+            return user;
+        }
+        else {
+            return null;
+        }
     }
 
 }
