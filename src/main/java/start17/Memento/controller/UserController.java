157dd94df0ad45388824febd7a071da019473d6f
jyjyeon <k-j_y@naver.com>
2023-02-24 21:37:23 +0900

    feat: 비밀번호 암호화

--- src/main/java/start17/Memento/controller/UserController.java
+++ src/main/java/start17/Memento/controller/UserController.java
@@ -7,15 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import start17.Memento.domain.User;
-import start17.Memento.model.dto.UserDto;
 import start17.Memento.service.UserService;
 
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Api(tags = "Users")
 @RestController
 @Slf4j
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -24,24 +23,11 @@ public class UserController {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @ApiOperation(value = "회원가입", notes = "회원 id, 비밀번호, 닉네임을 입력해 user 정보를 생성한다.")
-    @PostMapping("/users/register")
+    @PostMapping("/register")
     public String register(@RequestBody User user) throws IOException {
         userService.createUser(user);
         log.info("register success");
         return "Register success";
     }
 
-    @ApiOperation(value = "로그인", notes = "회원 id, 비밀번호를 입력해 로그인한다.")
-    @PostMapping("/users/login")
-    public String login(@RequestBody UserDto.LoginInfo user) throws IOException {
-
-        User login_user = userService.loginCheck(user);
-
-        if(login_user != null) {
-            log.info("login success");
-            return "Login success";
-        }
-        log.info("login fail");
-        return "redirect:/users/login";
-    }
 }
--- src/main/java/start17/Memento/domain/User.java
+++ src/main/java/start17/Memento/domain/User.java
@@ -1,10 +1,21 @@
 package start17.Memento.domain;
 
-import lombok.Data;
 
-@Data
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class User {
     private String user_id;
     private String password;
     private String nickname;
+
+    public User() {
+    }
+
+    public User(String user_id, String password, String nickname) {
+        this.user_id = user_id;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
