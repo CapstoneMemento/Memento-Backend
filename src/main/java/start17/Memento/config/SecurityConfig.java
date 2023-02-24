dfe7f79975746e8161c6daafb0b00f934ebab145
jyjyeon <k-j_y@naver.com>
2023-02-24 21:37:23 +0900

    feat: 비밀번호 암호화

---
+++ src/main/java/start17/Memento/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package start17.Memento.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/start17/Memento/service/UserService.java
+++ src/main/java/start17/Memento/service/UserService.java
@@ -2,10 +2,10 @@ package start17.Memento.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import start17.Memento.model.dao.UserMapper;
 import start17.Memento.domain.User;
-import start17.Memento.model.dto.UserDto;
 
 @Service
 @Slf4j
@@ -13,21 +13,18 @@ public class UserService {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public void createUser(User user) {
+        encryptPassword(user);
         userMapper.createUser(user);
     }
 
-    public User loginCheck(UserDto.LoginInfo user_dto) {
-
-        User user = userMapper.getUserById(user_dto.getUser_id());
-
-        if(user_dto.getPassword().equals(user.getPassword())) {
-            return user;
-        }
-        else {
-            return null;
-        }
+    private void encryptPassword(User user) {
+        String enPw = passwordEncoder.encode(user.getPassword());
+        user.setPassword(enPw);
     }
+
 
 }
