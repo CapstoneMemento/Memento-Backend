b6d41030dde4fad9ae140b819cc266885cddb66f
jyjyeon <k-j_y@naver.com>
2023-02-28 14:02:03 +0900

    Revert "feat: 로그인 틀 구현"
    This reverts commit bb6b528fbd53be6c9c2a8c232e28704722b02015.

--- src/main/java/start17/Memento/config/SecurityConfig.java
+++ src/main/java/start17/Memento/config/SecurityConfig.java
@@ -1,69 +1,25 @@
 package start17.Memento.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import start17.Memento.jwt.JwtAuthenticationFilter;
-import start17.Memento.jwt.JwtTokenProvider;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration
-    ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //Disable csrf to use token
         http
                 .cors().disable()
-                .csrf().disable();
-
-        http
-                .authorizeRequests()
-                .antMatchers(
-                        "/",
-                        "/users/register",
-                        "/users/login"
-                ).permitAll()
-                .anyRequest().authenticated()
-                        .and()
-                                .addFilterBefore(new JwtAuthenticationFilter( jwtTokenProvider),
-                                        UsernamePasswordAuthenticationFilter.class);
-
-        http
-                .formLogin().disable();
-
-        //No session will be created or useed by spring security
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-//        //JWT exception handling
-//        http
-//                .exceptionHandling()
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint);
-
-        //JWT 적용
-//        http
-//                .apply(new JwtSecurityConfig(jwtTokenProvider));
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
 
         return http.build();
     }
--- src/main/java/start17/Memento/domain/User.java
+++ src/main/java/start17/Memento/domain/User.java
@@ -1,70 +1,21 @@
 package start17.Memento.domain;
 
 
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter @Setter
-@Builder
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-public class User implements UserDetails {
-
-    @Id
-    @Column(nullable = false, unique = true)
+public class User {
     private String user_id;
-
-    @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false)
     private String nickname;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<String> roles = new ArrayList<>();
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+    public User() {
     }
 
-    @Override
-    public String getUsername() {
-        return this.user_id;
+    public User(String user_id, String password, String nickname) {
+        this.user_id = user_id;
+        this.password = password;
+        this.nickname = nickname;
     }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
 }
