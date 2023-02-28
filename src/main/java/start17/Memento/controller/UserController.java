bb6b528fbd53be6c9c2a8c232e28704722b02015
jyjyeon <k-j_y@naver.com>
2023-02-28 14:00:46 +0900

    feat: 로그인 틀 구현

--- src/main/java/start17/Memento/controller/UserController.java
+++ src/main/java/start17/Memento/controller/UserController.java
@@ -3,10 +3,12 @@ package start17.Memento.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import start17.Memento.domain.User;
+import start17.Memento.jwt.JwtTokenProvider;
 import start17.Memento.model.dto.UserDto;
 import start17.Memento.service.UserService;
 
@@ -14,13 +16,15 @@ import java.io.IOException;
 
 @Api(tags = "Users")
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
 
+    //회원가입(POST)
     @ApiOperation(value = "회원가입", notes = "회원 id, 비밀번호, 닉네임을 입력해 user 정보를 생성한다.")
     @PostMapping("/register")
     public String register(@RequestBody User user) throws IOException {
@@ -29,13 +33,14 @@ public class UserController {
         return "Register success";
     }
 
+    //로그인(POST)
     @ApiOperation(value = "로그인", notes = "회원 id, 비밀번호를 확인해 로그인한다.")
     @PostMapping("/login")
     public String login(@RequestBody UserDto.LoginInfo user) throws IOException {
         User loginUser = userService.loginCheck(user);
         if(loginUser != null) {
             log.info("login success");
-            return "Login success";
+            return jwtTokenProvider.createToken(loginUser.getUser_id(), loginUser.getRoles());
         }
         log.info("login fail");
         return "redirect:/login";
--- src/main/java/start17/Memento/service/UserService.java
+++ src/main/java/start17/Memento/service/UserService.java
@@ -1,5 +1,6 @@
 package start17.Memento.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,6 +9,7 @@ import start17.Memento.model.dao.UserMapper;
 import start17.Memento.domain.User;
 import start17.Memento.model.dto.UserDto;
 
+@RequiredArgsConstructor
 @Service
 @Slf4j
 public class UserService {
@@ -32,10 +34,19 @@ public class UserService {
         }
     }
 
+    public User findByUserId(String user_id) {
+        User user = userMapper.getUserById(user_id);
+        return user;
+    }
+
     private void encryptPassword(User user) {
         String enPw = passwordEncoder.encode(user.getPassword());
         user.setPassword(enPw);
     }
 
-
+//    @Override
+//    public UserDetails loadUserByUsername(String user_id) throws UsernameNotFoundException {
+//            return userRepository.findByUser_Id(user_id)
+//                    .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+//    }
 }
