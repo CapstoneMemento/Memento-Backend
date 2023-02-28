e3007c4123be9f947d3a700d2e73b0ca52b88e8f
jyjyeon <k-j_y@naver.com>
2023-02-28 14:00:46 +0900

    feat: 로그인 틀 구현

--- src/main/java/start17/Memento/domain/User.java
+++ src/main/java/start17/Memento/domain/User.java
@@ -1,21 +1,70 @@
 package start17.Memento.domain;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Getter @Setter
-public class User {
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @Column(nullable = false, unique = true)
     private String user_id;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String nickname;
 
-    public User() {
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
-    public User(String user_id, String password, String nickname) {
-        this.user_id = user_id;
-        this.password = password;
-        this.nickname = nickname;
+    @Override
+    public String getUsername() {
+        return this.user_id;
     }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
