64338b290e1546b643fb678dbb75b495ac0a1229
jyjyeon <k-j_y@naver.com>
2023-02-28 16:02:13 +0900

    Revert "feat: ㅠ.ㅠ"
    This reverts commit a188286549bb3bd54afdede684de40ad7f7f64fd.

--- src/main/java/start17/Memento/domain/User.java
+++ src/main/java/start17/Memento/domain/User.java
@@ -1,73 +1,21 @@
 package start17.Memento.domain;
 
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter @Setter
-public class User implements UserDetails {
-    @Id
+public class User {
     private String user_id;
-
-    @Column
     private String password;
-
-    @Column
     private String nickname;
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    private Role role;
+    public User() {
+    }
 
-    @Builder
-    public User(String user_id, String password, String nickname, Role role) {
+    public User(String user_id, String password, String nickname) {
         this.user_id = user_id;
         this.password = password;
         this.nickname = nickname;
-        this.role = role;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> roles = new HashSet<>();
-        roles.add(new SimpleGrantedAuthority(role.getValue()));
-        return roles;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.user_id;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
