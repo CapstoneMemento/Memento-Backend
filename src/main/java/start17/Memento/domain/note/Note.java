a21535af77ec3453d1bef1073c471db6ce4bb2b4
yoonsoodev <yoonsoo98@ewhain.net>
2023-03-07 21:34:24 +0900

    [feat] : 노트 기본 기능 - mybatis 이용

--- src/main/java/start17/Memento/domain/note/Note.java
+++ src/main/java/start17/Memento/domain/note/Note.java
@@ -13,16 +13,16 @@ public class Note {
     private String title; //노트 제목
     private String content; //노트 내용
     private String save_type; // 저장 타입 (ex: 두문자)
-    private String category; // 포함 목차
+    private int category_id; // 포함 목차
 
     public Note(){}
 
-    public Note(long id, String title, String content, String save_type, String category) {
+    public Note(long id, String title, String content, String save_type, int category_id) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.save_type = save_type;
-        this.category = category;
+        this.category_id = category_id;
     }
 
 }
--- src/main/java/start17/Memento/domain/note/NoteDetail.java
+++ src/main/java/start17/Memento/domain/note/NoteDetail.java
@@ -3,12 +3,8 @@ package start17.Memento.domain.note;
 import java.util.Date;
 
 public class NoteDetail {
-    private int note_id; // 대상 노트 id
-    private boolean playlist; //플레이리스트 포함 여부
-    private boolean quiz; // 퀴즈 여부
+    private int notes_id; // 대상 노트 id
     private Date date; //생성날짜
-    private boolean wrong; //오답 여부
-    private Integer scrap; // 스크랩수
     private String casenum; //사건번호
     private String court; // 법원 종류
     private String judge_type; // 판결 종류 (ex: 결정, 판결)
