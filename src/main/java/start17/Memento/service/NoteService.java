70eb82600163303d89f7807b9426d2df59e40f51
yoonsoodev <yoonsoo98@ewhain.net>
2023-03-10 20:55:06 +0900

    [feature] : Note 등록 (JPA 이용)

---
+++ src/main/java/start17/Memento/service/NoteService.java
@@ -0,0 +1,18 @@
+package start17.Memento.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import start17.Memento.model.dto.NoteSaveRequestDto;
+import start17.Memento.repository.NoteRepository;
+
+@RequiredArgsConstructor
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    @Transactional
+    public Long save(NoteSaveRequestDto requestDto){
+        return noteRepository.save(requestDto.toEntity()).getId();
+    }
+}
