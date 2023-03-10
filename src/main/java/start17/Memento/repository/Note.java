d303e2bc575eff24a65bb9b4b4d2b89c4a978eee
yoonsoodev <yoonsoo98@ewhain.net>
2023-03-10 20:52:45 +0900

    [feat] : Note Repository 생성 (JPA 이용)

---
+++ src/main/java/start17/Memento/repository/Note.java
@@ -0,0 +1,34 @@
+package start17.Memento.repository;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Getter
+@NoArgsConstructor
+@Entity
+//상세페이지에 변경 가능한 것들만 모아둠
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(columnDefinition = "varchar(100) default 'EMPTY'" )
+    private String title; //노트 제목
+    @Column(columnDefinition = "varchar(1000) default 'EMPTY'" )
+    private String content; //노트 내용
+    @Column
+    private String type; // 저장 타입 (ex: 두문자)
+    @Column
+    private int categories_id; // 포함 목차
+
+    @Builder
+    public Note(String title, String content, String type, int categories_id){
+        this.title = title;
+        this.content= content;
+        this.type = type;
+        this.categories_id = categories_id;
+    }
+}
