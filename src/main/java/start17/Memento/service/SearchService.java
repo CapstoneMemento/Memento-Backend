9ab0ca52a182ca2ec9a95f37d2526bf18a27d8b3
yoonsoodev <yoonsoo98@ewhain.net>
2023-03-12 00:14:37 +0900

    [refactor] search controller 및 service 구조 변경

--- src/main/java/start17/Memento/service/SearchService.java
+++ src/main/java/start17/Memento/service/SearchService.java
@@ -1,4 +1,4 @@
-package start17.Memento.service.search;
+package start17.Memento.service;
 
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
@@ -6,6 +6,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import start17.Memento.domain.search.CaseContent;
 import start17.Memento.domain.search.CaseInfo;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -17,7 +18,40 @@ import java.util.List;
 
 
 @Service
-public class SearchWithQueryService {
+public class SearchService {
+
+    public CaseContent getCaseContent() throws IOException{
+        String url = "https://www.law.go.kr/DRF/lawService.do?OC=yoonsoo98&target=prec&ID=228541&type=XML";
+
+        try{
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+            //페이지에 접근할 Document 객체 생성 -> url 읽어들임
+            Document doc = dBuilder.parse(url);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("PrecService");
+
+            Element eElement = (Element)nList.item(0);
+            CaseContent cc = CaseContent.builder()
+                    .title(getTagValue("사건명",eElement))
+                    .main(getTagValue("판결요지",eElement))
+                    .provision(getTagValue("참조조문",eElement))
+                    .sentence(getTagValue("판시사항",eElement))
+                    .reason(getTagValue("판례내용",eElement))
+                    .build();
+            return cc;
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     public List<CaseInfo> getCasesList(String query) throws IOException{
         String url = "http://www.law.go.kr/DRF/lawSearch.do?OC=yoonsoo98&target=prec&type=XML&query=";
@@ -81,9 +115,10 @@ public class SearchWithQueryService {
             t= test.getChildNodes();
             if((Node)t.item(0)!=null) {nValue=(Node)t.item(0);}
         }
-        if(nValue==null) return null;
+        if(nValue==null) return "";
         return nValue.getNodeValue();
     }
+
 
 }
 
