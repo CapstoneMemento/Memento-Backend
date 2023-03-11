73fbf09ba28d5a2299cad361cfdef9fec8319a0d
yoonsoodev <yoonsoo98@ewhain.net>
2023-03-11 22:37:52 +0900

    [feat] : Note 수정기능

--- src/main/java/start17/Memento/controller/NoteController.java
+++ src/main/java/start17/Memento/controller/NoteController.java
@@ -2,8 +2,13 @@ package start17.Memento.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import start17.Memento.model.dto.NoteSaveRequestDto;
+import start17.Memento.domain.note.Note;
+import start17.Memento.model.dto.note.NoteResponseDto;
+import start17.Memento.model.dto.note.NoteSaveRequestDto;
+import start17.Memento.model.dto.note.NoteUpdateRequestDto;
 import start17.Memento.service.NoteService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,12 +16,29 @@ import start17.Memento.service.NoteService;
 public class NoteController {
     private final NoteService noteService;
 
+    // 노트 등록
     @PostMapping("/add")
     public Long save(@RequestBody NoteSaveRequestDto requestDto){
         return noteService.save(requestDto);
     }
 
-    @PutMapping("/{id}")
+    //노트 수정
+    @PutMapping("/{id}/edit")
+    public Long update(@PathVariable Long id, @RequestBody NoteUpdateRequestDto requestDto){
+        return noteService.update(id, requestDto);
+    }
+    //노트 조회
 
+    @GetMapping("/{id}")
+    public NoteResponseDto findById (@PathVariable Long id){
+        return noteService.findById(id);
+    }
 
+    @GetMapping("/list")
+    public List<Note> findAll (){
+        return noteService.findAll();
+    }
+    @DeleteMapping("/deleteAll")
+    public void delete(){
+    }
 }
