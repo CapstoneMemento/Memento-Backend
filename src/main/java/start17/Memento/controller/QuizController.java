a9fad2d4e6b13094392b8701bb16610655d88958
yoonsoodev <yoonsoo98@ewhain.net>
2023-04-13 02:44:51 +0900

    [feat] quiz string index 수정

--- src/main/java/start17/Memento/controller/QuizController.java
+++ src/main/java/start17/Memento/controller/QuizController.java
@@ -1,5 +1,7 @@
 package start17.Memento.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +14,13 @@ import start17.Memento.service.NoteService;
 import start17.Memento.service.QuizService;
 
 import java.util.List;
-
+@Api(tags="Quiz")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
     private final QuizService quizService;
+    @ApiOperation(value ="퀴즈 리스트 가져오기", notes= "입력받은 카테고리 id에 대해 한글 배열로 된 키워드 반환")
     @GetMapping("/{categories_id}")
     public List<Quiz> getQuizList (@PathVariable int categories_id){
         return quizService.getQuizList(categories_id);
