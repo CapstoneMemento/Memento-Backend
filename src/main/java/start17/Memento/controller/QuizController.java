273ed1d26f8e899f8b3a56471d72d4ad50a0fa0a
yoonsoodev <yoonsoo98@ewhain.net>
2023-04-13 02:02:40 +0900

    [feat] Quiz 객체 추출

--- src/main/java/start17/Memento/controller/QuizController.java
+++ src/main/java/start17/Memento/controller/QuizController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import start17.Memento.domain.note.Note;
+import start17.Memento.domain.quiz.Quiz;
 import start17.Memento.model.dto.note.NoteResponseDto;
 import start17.Memento.service.NoteService;
+import start17.Memento.service.QuizService;
 
 import java.util.List;
 
@@ -15,9 +17,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
-    private final NoteService noteService;
+    private final QuizService quizService;
     @GetMapping("/{categories_id}")
-    public List<Note> getQuizList (@PathVariable int categories_id){
-        return noteService.findByCategoryID(categories_id);
+    public List<Quiz> getQuizList (@PathVariable int categories_id){
+        return quizService.getQuizList(categories_id);
     }
 }
