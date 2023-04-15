b3a1f82e1459ea7283411fed9f2a07e8e4ff71fd
yoonsoodev <yoonsoo98@ewhain.net>
2023-04-15 19:28:24 +0900

    [feat] keyword 찾기 및 수정 기능 추가

--- src/main/java/start17/Memento/domain/keyword/KeywordRepository.java
+++ src/main/java/start17/Memento/domain/keyword/KeywordRepository.java
@@ -1,10 +1,18 @@
 package start17.Memento.domain.keyword;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     List<Keyword> findByNoteid(@Param("noteid") Long noteid);
+    @Transactional
+    @Modifying
+    @Query("delete from Keyword k where k.noteid = :noteid")
+    void deleteByNoteid(@Param("noteid") Long noteid);
+
 }
--- src/main/java/start17/Memento/model/dto/keyword/KeywordResponseDto.java
+++ src/main/java/start17/Memento/model/dto/keyword/KeywordResponseDto.java
@@ -1,7 +1,20 @@
 package start17.Memento.model.dto.keyword;
 
 import lombok.Getter;
+import start17.Memento.domain.keyword.Keyword;
+import start17.Memento.domain.note.Note;
 
 @Getter
 public class KeywordResponseDto {
+    private Long id;
+    private Long noteid;
+    int first;
+    int last;
+
+    public KeywordResponseDto(Keyword entity){
+        this.id = entity.getId();
+        this.noteid = entity.getNoteid();
+        this.first = entity.getFirst();
+        this.last = entity.getLast();
+    }
 }
