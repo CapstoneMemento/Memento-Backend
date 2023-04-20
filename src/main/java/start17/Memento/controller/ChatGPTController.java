af2b8d72c5351d0e0954d26debd7563c2e777387
yoonsoodev <yoonsoo98@ewhain.net>
2023-04-20 21:47:04 +0900

    [refactor] API 문서 추가 및 SearchController 본문내용 query 수정

--- src/main/java/start17/Memento/controller/ChatGPTController.java
+++ src/main/java/start17/Memento/controller/ChatGPTController.java
@@ -1,5 +1,7 @@
 package start17.Memento.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import start17.Memento.model.dto.chatGPT.ChatGPTResponseDto;
 import start17.Memento.model.dto.chatGPT.QuestionRequestDto;
 import start17.Memento.service.ChatGPTService;
-
+@Api(tags = "ChatGPT")
 @RestController
 @RequestMapping("/keyword")
 public class ChatGPTController {
@@ -18,6 +20,7 @@ public class ChatGPTController {
         this.chatGPTService = chatGPTService1;
     }
 
+    @ApiOperation(value = "ChatGPT에게 질문하기" , notes = "question에 '해당 판례 본문 내용' + '\n위 글에서 각 문장 내에서 중요한 단어들을 ','로 구분해서 나열해줘' 넣어줌 ")
     @PostMapping("/question")
     public ChatGPTResponseDto sendQuestion(@RequestBody QuestionRequestDto requestDto){
         return chatGPTService.askQuestion(requestDto);
